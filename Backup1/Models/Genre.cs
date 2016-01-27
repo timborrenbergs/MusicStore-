f63ef078f6e64325e4d3e45103a5422c538f81ec
Tim Borrenbergs <tim@freshcotton.com>
2016-01-27 09:59:14 +0100

    initial commit

---
+++ Backup1/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore2.Models
+{
+    public class Genre
+    {
+        public string Name { get; set; }
+    }
+    
+    public class Album
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+    }
+    
+}
